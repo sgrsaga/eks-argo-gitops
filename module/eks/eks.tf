857645f4012ee01ff369fc1d11f628e612450b97
Jayathilaka_Sagara <sagara.jayathilaka@pearson.com>
2023-09-15 14:58:04 +0530

    ng-policy

--- module/eks/eks.tf
+++ module/eks/eks.tf
@@ -184,8 +184,8 @@ resource "aws_eks_node_group" "node_groups" {
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.ng-policy-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.ng-policy-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.ng-policy-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
