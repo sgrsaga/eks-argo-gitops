3ad7f79e99d90fcaee0861c8a73bff5a27194311
Jayathilaka_Sagara <sagara.jayathilaka@pearson.com>
2023-09-15 13:16:23 +0530

    chng 3

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ module "main_network" {
 module "eks_gitops_cluster" {
   source                  = "./module/eks"
   cluster_subnets         = module.main_network.private_subnet_list
-  cluster_security_group  = module.main_network.public_security_group
+  cluster_security_group  = [module.main_network.public_security_group]
   cluster_name            = var.cluster_name
 
   depends_on              = [ module.main_network ]
