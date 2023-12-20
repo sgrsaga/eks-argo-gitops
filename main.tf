67e314cf893ff28d3a0a07faa2511de20ecba984
Jayathilaka_Sagara <sagara.jayathilaka@pearson.com>
2023-12-20 18:17:43 +0530

    access via config filepath

--- main.tf
+++ main.tf
@@ -29,16 +29,17 @@ provider "helm" {
 }
 
 provider "kubernetes" {
-  # config_path    = var.config_path
-  # #config_context = "my-context"
-  host = module.k8s.eks_cluster_endpoint
-    
-  cluster_ca_certificate = base64decode(module.k8s.kubeconfig-certificate-authority-data) 
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1" 
-    args = ["eks", "get-token", "--cluster-name", var.cluster_name] 
-    command = "aws"
-  }
+  config_path    = var.config_path
+  #config_context = "my-context"
+
+  # Use Exec plugins
+  # host = module.k8s.eks_cluster_endpoint
+  # cluster_ca_certificate = base64decode(module.k8s.kubeconfig-certificate-authority-data) 
+  # exec {
+  #   api_version = "client.authentication.k8s.io/v1beta1" 
+  #   args = ["eks", "get-token", "--cluster-name", var.cluster_name] 
+  #   command = "aws"
+  # }
 }
 
 /*
