e052ea9691b3abd107964c9697937b113938bca6
Jayathilaka_Sagara <sagara.jayathilaka@pearson.com>
2023-12-22 21:59:24 +0530

    folder structure changed for environement and kustomize support

---
+++ terraform/environment/staging/main.tf
@@ -0,0 +1,136 @@
+## Add the provide section.
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.15.0" ## was 5.5.0
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.12.1"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.24.0"
+    }
+  }
+}
+
+## Providers
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = var.config_path
+  }
+}
+
+provider "kubernetes" {
+  config_path = var.config_path
+  #config_context = "my-context"
+
+  # Use Exec plugins
+  # host = module.k8s.eks_cluster_endpoint
+  # cluster_ca_certificate = base64decode(module.k8s.kubeconfig-certificate-authority-data) 
+  # exec {
+  #   api_version = "client.authentication.k8s.io/v1beta1" 
+  #   args = ["eks", "get-token", "--cluster-name", var.cluster_name] 
+  #   command = "aws"
+  # }
+}
+
+/*
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+*/
+
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-files-sgr-jay"
+    key    = "eks-argo-helm.tfstate"
+    region = "ap-south-1"
+  }
+}
+
+
+/*
+## Terraform Backend
+terraform {
+  backend "remote" {
+    organization = "home_org_sagara"
+    hostname = "app.terraform.io"
+    workspaces {
+      name = "k8s_infra"
+    }
+  }
+}
+*/
+
+
+# 1. Call the main network module
+module "main_network" {
+  source                         = "./module/main_network"
+  vpc_name                       = var.vpc_name
+  vpc_cidr                       = var.vpc_cidr
+  public_source_cidr             = var.public_source_cidr
+  public_source_cidr_v6          = var.public_source_cidr_v6
+  ig_name                        = var.ig_name
+  public_subnets                 = var.public_subnets
+  private_subnets                = var.private_subnets
+  public_access_sg_ingress_rules = var.public_access_sg_ingress_rules
+  public_rt                      = var.public_rt
+  private_rt                     = var.private_rt
+}
+
+
+
+# 2. EKS Cluster creation
+module "eks_gitops_cluster" {
+  source           = "./module/eks"
+  private_ng_size  = var.private_ng_size
+  public_ng_size   = var.public_ng_size
+  cluster_name     = var.cluster_name
+  node_group_names = var.node_group_names
+  node_group_size  = var.node_group_size
+
+  # Versions
+  # version                 = var.k8s_version
+  # cni-version             = var.cni-version
+  # coredns-version         = var.coredns-version
+  # kube-proxy-version      = var.kube-proxy-version
+  # ebs-csi-version         = var.ebs-csi-version
+
+  depends_on = [module.main_network]
+}
+
+# 3. Kubernetes resources 
+module "k8s" {
+  source     = "./module/k8s_resources"
+  ingress_ns = var.ingress_ns
+  argo_ns    = var.argo_ns
+  monitoring_ns = var.monitoring_ns
+
+  depends_on = [module.eks_gitops_cluster]
+}
+
+# 4. Install Helm based utilities for the EKS
+module "helm_repos" {
+  source     = "./module/helm"
+  ingress_ns = module.k8s.ingress_ns
+  argo_ns    = module.k8s.argo_ns
+  monitoring_ns = module.k8s.monitoring_ns
+
+  depends_on = [module.k8s]
+}
+
+# 5. Create EKS Access level profile Developer and Admin users for initate access
+module "eks_access" {
+  source     = "./module/eks_access"
+}
---
+++ terraform/environment/staging/variables.tf
@@ -0,0 +1,199 @@
+# 1.1. Create a VPC
+variable "vpc_name" {
+  type = string
+}
+variable "vpc_cidr" {
+  type = string
+}
+variable "public_source_cidr" {
+  type = list(string)
+}
+variable "public_source_cidr_v6" {
+  type = list(string)
+}
+
+
+# 2. Create a Internet Gateway
+variable "ig_name" {
+  type = string
+}
+
+# 1.3. Create 2 Route tables
+variable "public_rt" {
+  type = string
+}
+variable "private_rt" {
+  type = string
+}
+
+# 1.4. Create 3 Public Subnets
+variable "public_subnets" {
+  type = list(string)
+}
+
+# 1.5. Create 3 Private Subnets
+variable "private_subnets" {
+  type = list(string)
+}
+
+# 1.6. Create Public access Security Group
+variable "public_access_sg_ingress_rules" {
+  type = list(object({
+    from_port = number
+    to_port   = number
+    protocol  = string
+  }))
+  default = [
+
+  ]
+}
+
+
+### EKS Cluster variables
+# 1. EKS Cluster name
+variable "cluster_name" {
+  type    = string
+  default = "MyEKS_Cluster"
+
+}
+
+# 2. EKS Cluster subnets
+variable "cluster_subnets" {
+  type    = list(string)
+  default = ["sub1"]
+
+}
+
+# 3. EKS Cluster security group
+variable "cluster_security_group" {
+  type    = list(string)
+  default = ["sg1"]
+
+}
+
+# 4. Node Group Names
+variable "node_group_names" {
+  type    = list(string)
+  default = ["NG1"]
+}
+
+# 5. Node Group Thresholds
+variable "node_group_size" {
+  type        = list(number)
+  description = "Value should be desired_size,max_size, min_size and max_unavailable"
+  default     = [1, 2, 1, 1]
+}
+
+# 6. Public NG size
+variable "public_ng_size" {
+  type    = number
+  default = 1
+}
+# 7. Private NG size
+variable "private_ng_size" {
+  type    = number
+  default = 1
+}
+
+# # 8. K8S version to spin up
+# variable "k8s_version" {
+#     type = string 
+#     default = "1.28"
+# }
+
+
+# # 9. CNI version
+# variable "cni-version" {
+#     type = string  
+# }
+
+# # 10. CoreDNS version
+# variable "coredns-version" {
+#     type = string  
+# }
+
+# # 11. KubeProxy version
+# variable "kube-proxy-version" {
+#     type = string  
+# }
+
+# # 12. EBS CSI version
+# variable "ebs-csi-version" {
+#     type = string  
+# }
+
+
+### EC2 Nodes Parameter
+# 1. EC2 AMI
+variable "ami_id" {
+  type = string
+}
+# 2. Number of EC2s
+variable "ec2_node_cnt" {
+  type = number
+}
+# 3. SSH Key Name
+variable "ssh_key_name" {
+  type = string
+}
+
+# 4. Instance type
+variable "instance_type" {
+  type = string
+}
+
+# 5. Role Name
+variable "role_name" {
+  type = string
+}
+# 7. Userdata file
+variable "user_data_file" {
+  type = string
+}
+# 9. Volume size
+variable "root_sorage" {
+  type    = number
+  default = 20
+}
+
+# 10. Master Nodes
+variable "master_names" {
+  type    = list(string)
+  default = ["Master1"]
+}
+# 5. Master Type - master_names
+variable "master_type" {
+  type = string
+}
+
+# 11. Worker Nodes
+variable "worker_names" {
+  type    = list(string)
+  default = ["Worker_1", "Worker_2"]
+}
+# 5. Worker Type - worker_names
+variable "worker_type" {
+  type = string
+}
+######################### Helm module
+
+# Config path
+variable "config_path" {
+  type = string
+}
+
+# Ingress name spece
+variable "ingress_ns" {
+  type = string
+}
+
+# ArgoCD namespace
+variable "argo_ns" {
+  type    = string
+  default = "argo_ns"
+}
+
+# Monitoring  namespece
+variable "monitoring_ns"{
+    type = string
+}
