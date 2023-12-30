4c117db782d75f93eb8d1e8e53d0240b8646f0a5
Jayathilaka_Sagara <sagara.jayathilaka@pearson.com>
2023-12-30 13:59:28 +0530

    Commented output

--- terraform/module/k8s_resources/output.tf
+++ terraform/module/k8s_resources/output.tf
@@ -1,16 +1,16 @@
-# Ingress namesapce
-output "ingress_ns"{
-    value = kubernetes_namespace.ingress.metadata.0.name
-}
+# # Ingress namesapce
+# output "ingress_ns"{
+#     value = kubernetes_namespace.ingress.metadata.0.name
+# }
 
 
-# ArgoCD namesapce
-output "argo_ns"{
-    value = kubernetes_namespace.argo.metadata.0.name
-}
+# # ArgoCD namesapce
+# output "argo_ns"{
+#     value = kubernetes_namespace.argo.metadata.0.name
+# }
 
 
-# Monitoring namesapce
-output "monitoring_ns"{
-    value = kubernetes_namespace.monitoring.metadata.0.name
-}
+# # Monitoring namesapce
+# output "monitoring_ns"{
+#     value = kubernetes_namespace.monitoring.metadata.0.name
+# }
