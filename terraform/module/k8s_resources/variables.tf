f2b0eada894dcd1c507af2da23c225154ae3121f
Jayathilaka_Sagara <sagara.jayathilaka@pearson.com>
2023-12-31 11:51:56 +0530

    Spin up EKS cluster

--- terraform/module/k8s_resources/variables.tf
+++ terraform/module/k8s_resources/variables.tf
@@ -1,16 +1,16 @@
-# Ingress namespece
-variable "ingress_ns"{
-    type = string
-}
+# # Ingress namespece
+# variable "ingress_ns"{
+#     type = string
+# }
 
 
-# ArgoCD  namespece
-variable "argo_ns"{
-    type = string
-}
+# # ArgoCD  namespece
+# variable "argo_ns"{
+#     type = string
+# }
 
 
-# Monitoring  namespece
-variable "monitoring_ns"{
-    type = string
-}
+# # Monitoring  namespece
+# variable "monitoring_ns"{
+#     type = string
+# }
