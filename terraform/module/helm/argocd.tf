42998b2c64920a5e0b2524ad39f1205a116bb0e2
Jayathilaka_Sagara <sagara.jayathilaka@pearson.com>
2024-01-05 18:49:28 +0530

    argocd test

---
+++ terraform/module/helm/argocd.tf
@@ -0,0 +1,31 @@
+#######################################
+## Install required resources for ArgoCD with helm
+#######################################
+
+# Create namespace for argo
+resource "kubernetes_namespace" "argo" {  
+  metadata {
+    name = var.argo_ns
+  }
+}
+
+# Install ArgoCD helm chart
+resource "helm_release" "argocd" {
+    name = "argocd"
+    repository = "https://argoproj.github.io/argo-helm"
+    chart = "argo-cd"
+    namespace = kubernetes_namespace.argo.metadata.0.name
+    version = "5.51.6"
+
+    # Set replicas
+    set {
+        name  = "replicas"
+        value = "2"
+    }
+    # Set Node Selector to Utility nodes
+    set {
+        name = "nodeSelector.category"
+        value = "utility"
+        type = "string"
+    }
+}
--- terraform/module/helm/variables.tf
+++ terraform/module/helm/variables.tf
@@ -21,10 +21,10 @@ variable "ingress_ns"{
     type = string
 }
 
-# # ArgoCD  namespece
-# variable "argo_ns"{
-#     type = string
-# }
+# ArgoCD  namespece
+variable "argo_ns"{
+    type = string
+}
 
 
 # # Monitoring  namespece
