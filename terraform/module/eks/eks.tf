61cdb562d999fb4620c2f025d2673c925bef2fcf
Jayathilaka_Sagara <sagara.jayathilaka@pearson.com>
2024-01-09 22:17:16 +0530

    PREFER_NO_SCHEDULE

--- terraform/module/eks/eks.tf
+++ terraform/module/eks/eks.tf
@@ -250,7 +250,7 @@ resource "aws_eks_node_group" "node_groups2" {
   taint {
     key    = "utility"
     value  = "yes"
-    effect = "PreferNoSchedule"
+    effect = "PREFER_NO_SCHEDULE"
   }
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
