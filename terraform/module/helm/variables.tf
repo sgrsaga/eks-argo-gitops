72f1d10d95515e8ded5928c678474e9bc4d4ea0e
Jayathilaka_Sagara <sagara.jayathilaka@pearson.com>
2024-01-09 08:34:04 +0530

    Duplicate variable removed. Spin the cluster

--- terraform/module/helm/variables.tf
+++ terraform/module/helm/variables.tf
@@ -1,15 +1,3 @@
-# Ingress namespace
-variable "ingress_ns"{
-    type = string
-    default = "ingress"
-}
-
-# ArgoCD namespace
-variable "argo_ns"{
-    type = string
-    default = "argo_ns"
-}
-
 # Monitoring  namespece
 variable "monitoring_ns"{
     type = string
